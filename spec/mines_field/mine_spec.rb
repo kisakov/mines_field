4f50c90f16afaac5354bfc1683e05ba05d9e7a0c
kisakov <isakov90@gmail.com>
2017-04-18 16:06:36 -0700

    add mine specs

--- spec/mines_field/mine_spec.rb
+++ spec/mines_field/mine_spec.rb
@@ -3,17 +3,30 @@ require 'spec_helper'
 describe MinesField::Mine do
   let(:mine1) { described_class.new(1, 1, 1) }
   let(:mine2) { described_class.new(2, 2, 1) }
+  let(:mine3) { described_class.new(3, 4, 5) }
   subject { mine1 }
 
   describe '.detonate?' do
-    it { expect(subject.detonate?(mine2)).to be_falsy }
+    context 'when two mines located inside blast radius' do
+      it { expect(subject.detonate?(mine3)).to be_truthy }
+    end
+
+    context 'when two mines located outside blast radius' do
+      it { expect(subject.detonate?(mine2)).to be_falsy }
+    end
   end
 
   describe '.distance' do
-    it { expect(subject.send(:distance, mine2).round(4)).to eq 1.4142 }
+    let(:distance) { 1.4142 }
+    
+    it 'can be solved with Euclidean distance' do
+      expect(subject.send(:distance, mine2).round(4)).to eq distance
+    end
   end
 
   describe '.to_s' do
-    it { expect(subject.to_s).to eq("#{subject.x} #{subject.y} #{subject.radius}") }
+    it "display all record's attributes divided by empty space" do
+      expect(subject.to_s).to eq("#{subject.x} #{subject.y} #{subject.blast_radius}")
+    end
   end
 end
